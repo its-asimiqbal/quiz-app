b2c307520d900a0680e3faa453247a36857c062b
its-asimiqbal <its.asim.iqball@gmail.com>
2023-11-18 21:35:42 +0500

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,54 @@
+import 'package:adv_basics/question_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  final List<String> selectedAnswers = [];
+
+  void choosenAnswers (String answer){
+    selectedAnswers.add(answer);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if(activeScreen == 'questions-screen'){
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 93, 22, 168),
+                Color.fromARGB(255, 103, 33, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
