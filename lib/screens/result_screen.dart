e65cf195ac7ea188903ad37bfbfd67f148110e37
its-asimiqbal <its.asim.iqball@gmail.com>
2023-11-21 15:46:00 +0500

    files updated

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,12 +1,36 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/screens/question_screen.dart';
+import 'package:adv_basics/screens/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key, required this.chosenAnswer});
+  const ResultScreen({
+    super.key,
+    required this.chosenAnswer,
+  });
 
   final List<String> chosenAnswer;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswer[i]
+      });
+    }
+
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = 0;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -18,7 +42,7 @@ class ResultScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('The answers list'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
